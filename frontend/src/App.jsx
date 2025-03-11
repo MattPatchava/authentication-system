3ecfedd2c1741603495e48b6bf60d4995844bcda
Matthew Patchava <mattpatchava@hotmail.com>
2025-03-12 00:10:23 +1000

    Set up default handler for unknown routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/public/Home.jsx';
 import Login from './pages/public/Login.jsx';
 import Register from './pages/public/Register.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 import Dashboard from './pages/protected/Dashboard.jsx';
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
       </Routes>
   );
 }
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header.jsx';
+
+function NotFound() {
+    return (
+        <div>
+            <Header />
+            <h2>404: The requested resource could not be found.</h2>
+        </div>
+    );
+}
+
+export default NotFound;
