4df0f08b0b32cca369461920da0e88ffd3b4c7f2
Matt Patchava <mattpatchava@hotmail.com>
2025-03-22 23:05:46 +1000

    Amend Postgres initialisation scripts
    - Move .env.database into /backend directory for easier access from
      different modules
    - Create postgres.js module for pg connection establishment

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source .env.database
+source ../.env.database
 psql -h "${DB_HOST}" -p "${DB_PORT}" -U postgres <<EOF
 CREATE ROLE auth_app_user WITH LOGIN PASSWORD '${DB_PASSWORD}';
 CREATE DATABASE auth_db OWNER auth_app_user;
--- backend/scripts/schema.sql
+++ backend/scripts/schema.sql
@@ -1,7 +1,10 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    created_at TIMESTAMP
 );
 
 GRANT ALL ON ALL TABLES IN SCHEMA public TO auth_app_user;
