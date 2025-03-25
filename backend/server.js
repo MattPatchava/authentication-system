d46d5ee3528f5ed4f763d9dd0cafd6287f77e72b
Matthew Patchava <mattpatchava@hotmail.com>
2025-03-26 00:19:15 +1000

    Install TS modules, create tsconfig.json file

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,21 @@
+"use strict";
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-
 require('dotenv').config();
-
 const cors = require('cors');
 app.use(cors({
     origin: process.env.FRONTEND_URI,
     credentials: true,
 }));
-
 const pool = require('./config/postgres.js');
-
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swaggerConfig.js');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 app.use(express.json());
 app.use(cookieParser());
-
 const router = require('./routes/router.js');
 app.use('/', router);
-
 const PORT = process.env.SERVER_PORT;
 const INTERFACE = process.env.SERVER_IP;
 app.listen(PORT, INTERFACE, () => {
